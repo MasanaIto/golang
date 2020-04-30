73da9c3b3224ba4c666f0cb2ec29883ba96d6b17
MasanaIto <z3heng0326@gmail.com>
2020-05-01 00:07:26 +0900

    switch

--- 4_lesson/lesson.go
+++ 4_lesson/lesson.go
@@ -1,48 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func getOsName() string {
+	return "mac"
+}
 
 func main() {
-	l := []string{"python", "go", "java"}
-
-	for i := 0; i < len(l); i++ {
-		fmt.Println(i, l[i])
-		// 0 python
-		// 1 go
-		// 2 java
+	switch os := getOsName(); os {
+	case "mac":
+		fmt.Println("Mac")
+	case "windows":
+		fmt.Println("Windows")
+	default:
+		fmt.Println("Default!", os)
 	}
 
-	for i, v := range l {
-		fmt.Println(i, v)
-		// 0 python
-		// 1 go
-		// 2 java
-	}
-
-	for _, v := range l {
-		fmt.Println(v)
-		// python
-		// go
-		// java
-	}
-
-	m := map[string]int{"apple": 100, "banana": 200}
-
-	for k, v := range m {
-		fmt.Println(k, v)
-		// apple 100
-		// banana 200
-	}
-
-	for k := range m {
-		fmt.Println(k)
-		// apple
-		// banana
-	}
-
-	for _, v := range m {
-		fmt.Println(v)
-		// 100
-		// 200
+	t := time.Now()
+	fmt.Println(t.Hour())
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Morning!")
+	case t.Hour() < 17:
+		fmt.Println("afternoon")
 	}
 }
