173eec64eb61c8527edbdf07f29ffdce95822a33
MasanaIto <z3heng0326@gmail.com>
2020-05-01 01:00:37 +0900

    log

--- 4_lesson/lesson.go
+++ 4_lesson/lesson.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"log"
 	"os"
 )
 
-// defer ... main関数の処理が終わったら呼び出される
-
-func foo() {
-	defer fmt.Println("world")
-
-	fmt.Println("hello")
+func LoggingSettings(logFile string) {
+	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	multiLogFile := io.MultiWriter(os.Stdout, logfile)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	log.SetOutput(multiLogFile)
 }
 
-// lesson.go ファイルの 100byte分だけ出力する
 func main() {
-	foo()
-	file, _ := os.Open("./lesson.go")
-	// fileをオープンすると、クローズする処理を入れなくてはならない
-	defer file.Close()
-	data := make([]byte, 100)
-	file.Read(data)
-	fmt.Println(string(data))
+	LoggingSettings("test.log")
+	// test.log 実行結果
+	// 2020/05/01 00:59:32 lesson.go:20: logging
+	// 2020/05/01 00:59:32 lesson.go:21: string test
+
+	log.Println("logging")              // 2020/05/01 00:50:13 logging
+	log.Printf("%T %v", "test", "test") // 2020/05/01 00:50:13 string test
+
+	// log.Fatalln("error")
 }
