c4a60388e58d13810cff5534d1ffb682f8a0174b
MasanaIto <z3heng0326@gmail.com>
2020-05-01 01:11:53 +0900

    panicとrecover

--- 4_lesson/lesson.go
+++ 4_lesson/lesson.go
@@ -1,26 +1,22 @@
 package main
 
-import (
-	"io"
-	"log"
-	"os"
-)
+import "fmt"
 
-func LoggingSettings(logFile string) {
-	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	multiLogFile := io.MultiWriter(os.Stdout, logfile)
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	log.SetOutput(multiLogFile)
+func thirdPartyConnectDB() {
+	// プログラム強制終了する, 本来は自分でpanicを書かない
+	panic("Unable to connect database!")
+}
+
+func save() {
+	// panicをキャッチして、recoverによりプログラムの強制終了を起こさせない
+	defer func() {
+		s := recover()
+		fmt.Println(s)
+	}()
+	thirdPartyConnectDB()
 }
 
 func main() {
-	LoggingSettings("test.log")
-	// test.log 実行結果
-	// 2020/05/01 00:59:32 lesson.go:20: logging
-	// 2020/05/01 00:59:32 lesson.go:21: string test
-
-	log.Println("logging")              // 2020/05/01 00:50:13 logging
-	log.Printf("%T %v", "test", "test") // 2020/05/01 00:50:13 string test
-
-	// log.Fatalln("error")
+	save()
+	fmt.Println("OK?")
 }
