f04c90c04ec153b5fb82fa49eea05b661f2f9be5
MasanaIto <z3heng0326@gmail.com>
2020-05-05 01:43:52 +0900

    カスタムエラー

--- 6_lesson/lesson.go
+++ 6_lesson/lesson.go
@@ -2,17 +2,29 @@ package main
 
 import "fmt"
 
-type Person struct {
-	Name string
-	Age  int
+/*
+UserNotFound ...
+*/
+type UserNotFound struct {
+	Username string
 }
 
-// struct の Person を書き換えたい場合、Stringerを使う
-func (p Person) String() string {
-	return fmt.Sprintf("My Name is %v.", p.Name)
+// エラーを作る時は、ポインタで*, ＆を付けるのが慣習
+func (e *UserNotFound) Error() string {
+	return fmt.Sprintf("User not found: %v", e.Username)
+}
+
+func myFunc() error {
+	// 他のエラー
+	ok := false
+	if ok {
+		return nil
+	}
+	return &UserNotFound{Username: "Masana"}
 }
 
 func main() {
-	masana := Person{"Masana", 20}
-	fmt.Println(masana) // My Name is Masana.
+	if err := myFunc(); err != nil {
+		fmt.Println(err) // User not found: Masana
+	}
 }
