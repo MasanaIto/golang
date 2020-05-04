8eb7f242f5f32ad7659dc85eaabaa292660fd53f
MasanaIto <z3heng0326@gmail.com>
2020-05-05 00:42:59 +0900

    struct

--- 5_lesson/lesson.go
+++ 5_lesson/lesson.go
@@ -2,22 +2,37 @@ package main
 
 import "fmt"
 
+type Vertex struct {
+	// 宣言は外部読み込みする場合は大文字でかく
+	X int
+	Y int
+	// X, Y int でもok
+	S string
+}
+
 func main() {
+	v := Vertex{X: 1, Y: 2}
+	fmt.Println(v)        // {1 2}
+	fmt.Println(v.X, v.Y) // 1 2
+	v.X = 100
+	fmt.Println(v.X, v.Y) // 100 2
 
-	// make は ポインタを返さない
-	s := make([]int, 0)
-	fmt.Printf("%T\n", s) // []int
+	v2 := Vertex{X: 1}
+	fmt.Println(v2) // {1 0}
 
-	m := make(map[string]int)
-	fmt.Printf("%T\n", m) // map[string]int
+	v3 := Vertex{1, 2, "test"}
+	fmt.Println(v3) // {1 2 test}
 
-	ch := make(chan int) // chan int
-	fmt.Printf("%T\n", ch)
+	v4 := Vertex{}
+	fmt.Println(v4) // {0 0 }
 
-	// new は ポインタを返す
-	var p *int = new(int)
-	fmt.Printf("%T\n", p) // *int
+	var v5 Vertex
+	fmt.Printf("%T %v\n", v5, v5) // main.Vertex &{0 0 }
 
-	var st = new(struct{}) // *struct {}
-	fmt.Printf("%T\n", st)
+	v6 := new(Vertex)
+	fmt.Printf("%T %v\n", v6, v6) // *main.Vertex &{0 0 }
+
+	v7 := &Vertex{}
+	fmt.Printf("%T %v\n", v7, v7) // *main.Vertex &{0 0 }
+
 }
