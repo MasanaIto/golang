51a935e37e0fa6b9c8e935b47c2a0daddfdbd987
MasanaIto <z3heng0326@gmail.com>
2020-05-05 15:59:52 +0900

    channel

--- 7_lesson/lesson.go
+++ 7_lesson/lesson.go
@@ -1,29 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
-func goroutine(s string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for i := 0; i < 5; i++ {
-		// time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
+// routineの間のデータやりとりができない、並列処理なのでreturnができないので、チャネルを作る
+
+func goroutine1(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
 	}
+	c <- sum
 }
 
-func normal(s string) {
-	for i := 0; i < 5; i++ {
-		// time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
+func goroutine2(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum -= v
 	}
+	c <- sum
 }
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go goroutine("world", &wg)
-	normal("hello")
-	wg.Wait()
+	s := []int{1, 2, 3, 4, 5}
+	c := make(chan int) // -15 15
+	go goroutine1(s, c)
+	go goroutine2(s, c)
+	x := <-c
+	fmt.Println(x)
+	y := <-c
+	fmt.Println(y)
 }
