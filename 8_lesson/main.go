5ee7740e643a21f64d79642e87bc1349b01a64d2
MasanaIto <z3heng0326@gmail.com>
2020-05-21 01:26:08 +0900

    package

---
+++ 8_lesson/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"Project/mylib"
+	"Project/mylib/under"
+	"fmt"
+)
+
+func main() {
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(mylib.Average(s))
+
+	mylib.Say()
+	under.Hello()
+}
