7b3772db4dd0863f5266b34f16d09348bd329241
MasanaIto <z3heng0326@gmail.com>
2020-05-24 01:40:34 +0900

    Web Application 1 - ioutil
    txtファイルを生成するプログラム

---
+++ 13_web_applications/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "io/ioutil"
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "test", Body: []byte("This is a sample Page.")}
+	p1.save()
+}
