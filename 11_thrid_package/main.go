b0a0fea879df3972974f4f4016bee57fe6370338
MasanaIto <z3heng0326@gmail.com>
2020-05-24 00:17:14 +0900

    config.iniの設定ファイルを読み込む

--- 11_thrid_package/main.go
+++ 11_thrid_package/main.go
@@ -1,42 +1,36 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
 
-	"golang.org/x/sync/semaphore"
+	"gopkg.in/ini.v1"
 )
 
-var s *semaphore.Weighted = semaphore.NewWeighted(1)
+type ConfigList struct {
+	Port      int
+	DbName    string
+	SQLDriver string
+}
 
-func longProcess(ctx context.Context) {
-	isAcquire := s.TryAcquire(1)
-	if !isAcquire {
-		fmt.Println("Could not get lock")
-		return
+var Config ConfigList
+
+func init() {
+	cfg, _ := ini.Load("config.ini")
+	Config = ConfigList{
+		Port:      cfg.Section("web").Key("port").MustInt(),
+		DbName:    cfg.Section("db").Key("name").MustString("example.sql"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
 	}
-	defer s.Release(1)
-	fmt.Println("Wait...")
-	time.Sleep(1 * time.Second)
-	fmt.Println("Done")
 }
 
 func main() {
-	ctx := context.TODO()
-	go longProcess(ctx)
-	go longProcess(ctx)
-	go longProcess(ctx)
-	time.Sleep(2 * time.Second)
-	go longProcess(ctx)
-	time.Sleep(5 * time.Second)
+	fmt.Printf("%T %v\n", Config.Port, Config.Port)
+	fmt.Printf("%T %v\n", Config.DbName, Config.DbName)
+	fmt.Printf("%T %v\n", Config.SQLDriver, Config.SQLDriver)
+
 }
 
-// 出力結果
-//
-// Wait...
-// Could not get lock
-// Could not get lock
-// Done
-// Wait...
-// Done
+// 実行結果 ... config.iniを読み込んでいる
+// int 8080
+// string stockdata.sql
+// string sqlite3
